f3ed5fb11ea2563f0de11d65ca376cfe73c0a1d3
Sérgio Rodrigues <sergiorodrigues.ti@gmail.com>
2020-09-28 09:21:54 -0300

    Teste Erro

--- src/main/java/br/ce/wcaquino/entidades/Filme.java
+++ src/main/java/br/ce/wcaquino/entidades/Filme.java
@@ -33,7 +33,7 @@ public class Filme {
 	public Double getPrecoLocacao() {
 		return precoLocacao;
 	}
-	public void setPrecoLocacao(Double precoLocacao) {
+	public void setPrecoLocaca(ouble precoLocacao) {
 		this.precoLocacao = precoLocacao;
 	}
 }
