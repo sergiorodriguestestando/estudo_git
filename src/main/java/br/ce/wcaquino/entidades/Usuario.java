1c17e801d53b9151474d8c7e25ca273bdf8c0f27
Sérgio Rodrigues <sergiorodrigues.ti@gmail.com>
2020-11-03 16:36:00 -0200

    Novo campo endereço

--- src/main/java/br/ce/wcaquino/entidades/Usuario.java
+++ src/main/java/br/ce/wcaquino/entidades/Usuario.java
@@ -3,9 +3,19 @@ package br.ce.wcaquino.entidades;
 public class Usuario {
 
 	private String nome;
-	
-	public Usuario() {}
-	
+	private String endereco;
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public Usuario() {
+	}
+
 	public Usuario(String nome) {
 		this.nome = nome;
 	}
